f3db4353be05af02dbd23ee03cefb9825c1c9498
Сергей <bondarevser.bondarev@yandex.ru>
2023-07-16 17:10:37 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws WrongLoginException {
         try {
-            Validator.check("asd", "abc", "abc");
+            Validator.check("aert", "hgffd", "fdgdghjnbvfrtedcvgt");
             System.out.println("Логин успешно принят");
         }catch (WrongLoginException e){
             throw new RuntimeException(e);
